fd54b189ccf97ed3cbd1d8641350bf4a2e114038
grauziitisos <85872382+grauziitisos@users.noreply.github.com>
2023-03-13 18:36:59 +0200

    init canvas tutuor

---
+++ app/src/main/java/com/aal/bb/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.aal.bb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val cnvsview = cnvsview(this)
+        cnvsview.contentDescription = "hello wordl:::::"
+        setContentView(cnvsview)
+    }
+}
---
+++ app/src/main/java/com/aal/bb/cnvsview.kt
@@ -0,0 +1,7 @@
+package com.aal.bb
+import android.content.Context
+import android.view.View
+
+class cnvsview (context: Context) : View(context)  {
+
+}
